452ceed53ddaf62033182f67c81bfa872d67a35b
tomytempleton <63156083+tomytempleton@users.noreply.github.com>
2020-11-05 15:45:43 +0300

    Update hello-word.py

--- hello-word.py
+++ hello-word.py
@@ -1,5 +1,5 @@
 <<<<<<< HEAD
 print('Hello Tomy and World for jenkins job new')
 =======
-print('Hello World for jenkins job new........................................')
+print('Hello World for jenkins job new haddiiii....')
 >>>>>>> ebc4aa63ca79b4bbe451cf86922f5377de829033
